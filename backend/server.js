8281a5ffb220f86fc43a89c53e793196e0585e74
Muhammad Ammar Ibrahim <96951639+chammar55@users.noreply.github.com>
2024-12-22 20:57:29 +0500

    backend complete

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -13,6 +18,11 @@ app.use(express.json()); // allow to get data from body of request
 app.use(cookieParser()); // For accessing the cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port http://localhost" + PORT);
